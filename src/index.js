1088ba30883ba81e086a28a862b9baaf20153050
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-01-20 18:29:05 -0500

    added new component TaskList

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+function TaskList() {
+  return (
+    <ul>
+      <li>Task 1</li>
+      <li>Task 2</li>
+      <li>Task 3</li>
+    </ul>
+  );
+}
+
 function App() {
   const p_style = {
     "marginBottom": "15px"
@@ -13,12 +23,7 @@ function App() {
       <header className="app-header">
         <h1>To-do List</h1>
         <p style={p_style}>Simple to-do list react-app</p>
-
-        <ul>
-          <li>Task 1</li>
-          <li>Task 2</li>
-          <li>Task 3</li>
-        </ul>
+        <TaskList/>
       </header>
     </div>
   )
