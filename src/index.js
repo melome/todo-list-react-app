50bfb0788d7d8235d25d9325dc3a752abba8075a
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-01-20 18:08:05 -0500

    added import css styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function HelloWorld() {
-  return <h1>Hello, World!</h1>;
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>To-do List</h1>
+        <p>Simple to-do list react-app</p>
+
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+        </ul>
+      </header>
+    </div>
+  )
 }
 
-ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById("root"));
+ReactDOM.render(<App></App>, document.getElementById("root"));
