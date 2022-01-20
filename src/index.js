96aa17ecf26b90f89094b6f2f124f57b2e327903
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-01-20 18:12:38 -0500

    added jsx inline styling

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
+  const p_style = {
+    "marginBottom": "15px"
+  };
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>To-do List</h1>
-        <p>Simple to-do list react-app</p>
+        <p style={p_style}>Simple to-do list react-app</p>
 
         <ul>
           <li>Task 1</li>
