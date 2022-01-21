7fb864714acf024b00c9cacaffe95c8ad305ded2
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-01-21 02:24:33 -0500

    Add task on blur

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,19 +2,25 @@ import { useState } from "react";
 import Task from "./Task"
 
 function TaskList() {
-  const taskItemList = [
+  const [taskItemList, setTaskItemList] = useState([
     "Task 1",
     "Task 2",
     "Task 3"
-  ];
+  ]);
 
-  const [taskValue, setTaskValue] = useState("Task N");
+  const [taskValue, setTaskValue] = useState("");
 
-  console.log(taskValue)
+  // console.log(taskValue)
   
   const inputHandler = (e) => {
     setTaskValue(e.target.value);
   }
+
+  const addTaskHandler = () => {
+    setTaskItemList([...taskItemList, taskValue]); //spread enumerator: spreaad values in an array
+    setTaskValue("");
+  };
+
   return (
     <div>
       
@@ -22,7 +28,11 @@ function TaskList() {
         <input 
           className="task-input" 
           placeholder="Create new task"
-          onChange={inputHandler}/>
+          onChange={inputHandler}
+          onBlur={addTaskHandler}
+          value={taskValue}
+        />
+
         {taskItemList.map((task, index) => {
           return <Task key={index} taskName={task}></Task>;
         })}
